b49cb987717eec96d5393eb64160f0956861af9d
GherMa <madalinghergut@gmail.com>
2025-03-24 11:32:01 +0200

    -keye noua

--- main/src/main/java/ro/tuiasi/ac/App.java
+++ main/src/main/java/ro/tuiasi/ac/App.java
@@ -1,13 +1,6 @@
 package ro.tuiasi.ac;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import services.ChatGPTService;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 public class App {
     public static void main(String[] args) {
